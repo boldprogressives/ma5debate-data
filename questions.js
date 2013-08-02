9b1025c2055c37862fe13deb5de3e5eafbd20b56
Ethan Jucovy <ejucovy@gmail.com>
2013-08-02 14:41:52 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -1,39 +1,4 @@
 opendebate.processFetchedQuestions({
   "iface": "list", 
-  "entries": [
-    {
-      "city": "Great Neck", 
-      "first_name": "Ethan", 
-      "last_name": "Jucovy", 
-      "user_id": 1195198, 
-      "submission": "Will you do a great job in Congress?", 
-      "district": "NY_03", 
-      "created": "2013-08-02T14:34:37", 
-      "state_votes": 0, 
-      "votes": 0, 
-      "district_votes": 0, 
-      "state": "NY", 
-      "iface": "question", 
-      "y": 40.786700000000003, 
-      "x": -73.725800000000007, 
-      "id": 16169379
-    }, 
-    {
-      "city": "Great Neck", 
-      "first_name": "Ethan", 
-      "last_name": "Jucovy", 
-      "user_id": 1195198, 
-      "submission": "Do you believe in global warming?", 
-      "district": "NY_03", 
-      "created": "2013-08-02T14:27:22", 
-      "state_votes": 0, 
-      "votes": 1, 
-      "district_votes": 0, 
-      "state": "NY", 
-      "iface": "question", 
-      "y": 40.786700000000003, 
-      "x": -73.725800000000007, 
-      "id": 16169370
-    }
-  ]
+  "entries": []
 });
