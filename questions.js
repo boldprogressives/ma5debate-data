ec0c91aafb94c8ec700fc22683dc48207a8600c8
Ethan Jucovy <ejucovy@gmail.com>
2013-08-02 14:27:39 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -1,4 +1,21 @@
 opendebate.processFetchedQuestions({
   "iface": "list", 
-  "entries": []
+  "entries": [
+    {
+      "city": "Great Neck", 
+      "first_name": "Ethan", 
+      "last_name": "Jucovy", 
+      "submission": "Do you believe in global warming?", 
+      "district": "NY_03", 
+      "created": "2013-08-02T14:27:22", 
+      "state_votes": 0, 
+      "votes": 0, 
+      "district_votes": 0, 
+      "state": "NY", 
+      "iface": "question", 
+      "y": 40.786700000000003, 
+      "x": -73.725800000000007, 
+      "id": 16169370
+    }
+  ]
 });
