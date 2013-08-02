aebc6dfa402a00cbc1dfdd61cba83552aac95481
Ethan Jucovy <ejucovy@gmail.com>
2013-08-02 14:48:07 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -1,4 +1,22 @@
 opendebate.processFetchedQuestions({
   "iface": "list", 
-  "entries": []
+  "entries": [
+    {
+      "city": "Ottawa", 
+      "first_name": "Drew", 
+      "last_name": "McConville", 
+      "user_id": 193927, 
+      "submission": "What will you do in Washington to help public transportation in Massachusetts?", 
+      "district": "IL_16", 
+      "created": "2013-08-02T14:47:18", 
+      "state_votes": 0, 
+      "votes": 0, 
+      "district_votes": 0, 
+      "state": "IL", 
+      "iface": "question", 
+      "y": 41.333599999999997, 
+      "x": -88.878100000000003, 
+      "id": 16169418
+    }
+  ]
 });
