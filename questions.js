c9507da5c3929df1ce734dd1351f0e0b5bddd421
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 17:07:30 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -51,23 +51,6 @@ opendebate.processFetchedQuestions({
       "y": 42.415199999999999, 
       "x": -71.005300000000005, 
       "id": 16172172
-    }, 
-    {
-      "city": "cambridge", 
-      "first_name": "Gail", 
-      "last_name": "Shulman", 
-      "user_id": 1653745, 
-      "submission": "Will you pledge to oppose cuts to Social Security and Medicare (and sign on to expand these programs)?", 
-      "district": "MA_05", 
-      "created": "2013-08-03T16:26:36", 
-      "state_votes": 0, 
-      "votes": 1, 
-      "district_votes": 0, 
-      "state": "MA", 
-      "iface": "question", 
-      "y": 42.377499999999998, 
-      "x": -71.131200000000007, 
-      "id": 16172168
     }
   ]
 });
