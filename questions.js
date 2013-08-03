2cb8d569318aec99086f7b87e3f35302a1d417ab
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 17:32:06 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -3,6 +3,23 @@ opendebate.processFetchedQuestions({
   "entries": [
     {
       "city": "Cambridge", 
+      "first_name": "Lois", 
+      "last_name": "Fine", 
+      "user_id": 251070, 
+      "submission": "What is your position on the validity of the Citizens United decision that corporations have the same rights as citizens?", 
+      "district": "MA_05", 
+      "created": "2013-08-03T17:31:52", 
+      "state_votes": 0, 
+      "votes": 0, 
+      "district_votes": 0, 
+      "state": "MA", 
+      "iface": "question", 
+      "y": 42.392299999999999, 
+      "x": -71.131299999999996, 
+      "id": 16172265
+    }, 
+    {
+      "city": "Cambridge", 
       "first_name": "H.", 
       "last_name": "Fleishon", 
       "user_id": 682229, 
