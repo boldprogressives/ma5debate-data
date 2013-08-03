40f8e73d5ba8c83c9e3ce0b1ac8a99e7338a55f9
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 22:38:06 +0000

    Updating data

--- votes.js
+++ votes.js
@@ -11,19 +11,7 @@ opendebate.processFetchedVotes({
       "state": "MT", 
       "iface": "vote", 
       "id": 16172782, 
-      "question_id": "16172696"
-    }, 
-    {
-      "city": "Helena", 
-      "first_name": "Bob", 
-      "last_name": "Balhiser", 
-      "user_id": 674040, 
-      "district": "MT_01", 
-      "created": "2013-08-03T22:33:50", 
-      "state": "MT", 
-      "iface": "vote", 
-      "id": 16172782, 
-      "question_id": "16172777"
+      "question_id": "16172726"
     }, 
     {
       "city": "Dolton", 
