c306a1edde0cdb6b3d1d61b29fa4fb77c2ca8d96
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 16:50:08 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -44,7 +44,7 @@ opendebate.processFetchedQuestions({
       "district": "MA_05", 
       "created": "2013-08-03T16:26:36", 
       "state_votes": 0, 
-      "votes": 0, 
+      "votes": 1, 
       "district_votes": 0, 
       "state": "MA", 
       "iface": "question", 
--- votes.js
+++ votes.js
@@ -242,6 +242,18 @@ opendebate.processFetchedVotes({
       "question_id": "16169418"
     }, 
     {
+      "city": "Ottawa", 
+      "first_name": "Drew", 
+      "last_name": "McConville", 
+      "user_id": 193927, 
+      "district": "IL_16", 
+      "created": "2013-08-02T16:58:57", 
+      "state": "IL", 
+      "iface": "vote", 
+      "id": 16169670, 
+      "question_id": "16172168"
+    }, 
+    {
       "city": "Great Neck", 
       "first_name": "Ethan", 
       "last_name": "Jucovy", 
