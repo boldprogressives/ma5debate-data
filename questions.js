633c3c89eab853f464a88488824ba4258338d838
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 16:09:10 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -1,22 +1,4 @@
 opendebate.processFetchedQuestions({
   "iface": "list", 
-  "entries": [
-    {
-      "city": "Great Neck", 
-      "first_name": "Ethan", 
-      "last_name": "Jucovy", 
-      "user_id": 385575, 
-      "submission": "test", 
-      "district": "NY_03", 
-      "created": "2013-08-03T15:59:44", 
-      "state_votes": 0, 
-      "votes": 0, 
-      "district_votes": 0, 
-      "state": "NY", 
-      "iface": "question", 
-      "y": 40.786700000000003, 
-      "x": -73.725800000000007, 
-      "id": 16172132
-    }
-  ]
+  "entries": []
 });
