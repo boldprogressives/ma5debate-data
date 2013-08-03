906266ab9587cc385ca4480a6b5ca143a3ee52cf
Ethan Jucovy <ejucovy@gmail.com>
2013-08-03 16:30:10 +0000

    Updating data

--- questions.js
+++ questions.js
@@ -2,6 +2,23 @@ opendebate.processFetchedQuestions({
   "iface": "list", 
   "entries": [
     {
+      "city": "Revere", 
+      "first_name": "Sam", 
+      "last_name": "Brodie", 
+      "user_id": 1191399, 
+      "submission": "Should the US be investing more in education? If so, what should we invest in (pre-school, public universities, technology upgrades for high schools, etc.) and how should we pay for it?", 
+      "district": "MA_05", 
+      "created": "2013-08-03T16:28:41", 
+      "state_votes": 0, 
+      "votes": 0, 
+      "district_votes": 0, 
+      "state": "MA", 
+      "iface": "question", 
+      "y": 42.415199999999999, 
+      "x": -71.005300000000005, 
+      "id": 16172172
+    }, 
+    {
       "city": "cambridge", 
       "first_name": "Gail", 
       "last_name": "Shulman", 
