ac9fc261381acb7c6de69b616dc02b4ba31f5595
Ethan Jucovy <ejucovy@gmail.com>
2013-08-04 03:32:04 +0000

    Updating data

--- votes.js
+++ votes.js
@@ -11,7 +11,7 @@ opendebate.processFetchedVotes({
       "state": "OR", 
       "iface": "vote", 
       "id": 16173421, 
-      "question_id": "16173222"
+      "question_id": "16173207"
     }, 
     {
       "city": "Eugene", 
