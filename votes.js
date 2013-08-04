142bd8b88870b9d49188c2879637913407d70def
Ethan Jucovy <ejucovy@gmail.com>
2013-08-04 14:26:04 +0000

    Updating data

--- votes.js
+++ votes.js
@@ -11,7 +11,7 @@ opendebate.processFetchedVotes({
       "state": "CO", 
       "iface": "vote", 
       "id": 16173909, 
-      "question_id": "16173512"
+      "question_id": "16173438"
     }, 
     {
       "city": "Westland", 
